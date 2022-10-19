efb59a5142a1ddf6f0d73689b5fcc2997c989ed0
Filip Štěpánek <stepafil@gmail.com>
2022-10-19 20:27:18 +0200

    HW1 Cleanup

--- hw1/src/Storage.cpp
+++ hw1/src/Storage.cpp
@@ -11,7 +11,7 @@ typedef vector<vector<int>> graph;
 
 int calculateFrankenstein(const vector<int> &wordA, const vector<int> &wordB);
 
-int kruskal(graph &graph, const size_t& nodesCount);
+int primPQ(graph &graph, const size_t& nodesCount);
 
 using namespace std;
 
@@ -19,14 +19,20 @@ using namespace std;
 int main(int argc, char *argv[]) {
     auto programArguments = ProgramArguments::Parse(argc, argv);
 
+    //Read records
     vector<vector<int>> records = readRecords(programArguments.mInputFilePath);
     const size_t nodesCount = records.size();
 
+    //Begin time measurement
     auto start = high_resolution_clock::now();
 
+
+    //Initialize adjacency matrix
     graph graph(nodesCount, vector<int>(nodesCount));
+
+    //Execute the loop in parallel for each word
     int i,j;
-    #pragma omp parallel for schedule(static, 1) private(j)
+    #pragma omp parallel for schedule(dynamic, 1)  private(j)
     for (i = 0; i < nodesCount - 1; ++i) {
         for (j = i + 1; j < nodesCount; ++j) {
             int distance = calculateFrankenstein(records[i], records[j]);
@@ -35,84 +41,80 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    //Measure time to graph preparation
     auto stopGraph = high_resolution_clock::now();
     auto durationGraph = duration_cast<milliseconds>(stopGraph - start);
-
     cout << durationGraph.count() << " ms to create graph" << endl;
 
-    int treeCost = kruskal(graph, nodesCount);
-
+    //Calculate the prim tree cost
+    int treeCost = primPQ(graph, nodesCount);
     cout << treeCost << endl;
 
+    //Measure time to completion
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
-    cout << duration.count() << " ms to kruskal" << endl;
+    cout << duration.count() << " ms to primPQ" << endl;
 
+    //Write the result
     writeCost(treeCost, programArguments.mOutputFilePath);
 
     return 0;
 }
 
 int calculateFrankenstein(const vector<int> &wordA, const vector<int> &wordB) {
-    if (wordA.empty()) {
+    //If word A is empty no point to calculate anything
+    if (wordA.empty())
         return wordB.size();
-    }
 
-    if (wordB.empty()) {
+    //If word B is empty no point to calculate anything
+    if (wordB.empty())
         return wordA.size();
-    }
 
-    const int arr_size = wordA.size() + 1;
-    int lineA[arr_size];
-    int lineB[arr_size];
+    //Store word A length and use it as an X dimension size
+    const int lineLength = wordA.size() + 1;
+    int lineA[lineLength];
+    int lineB[lineLength];
 
-    for (int i = 0; i < arr_size; i++) {
+    //Fill first line with 1,2,...,lineLength
+    for (int i = 0; i < lineLength; i++) {
         lineA[i] = i;
     }
 
+    //Go vertically
     for (int i = 1; i <= wordB.size(); i++) {
         lineB[0] = i;
         const int letterB = wordB[i - 1];
 
+        //Go horizontally
         for (int j = 1; j <= wordA.size(); ++j) {
             const int letterA = wordA[j - 1];
 
+            //Check if letters are equal
+            int cost = letterA == letterB ? 0 : 1;
 
-            int cost;
-            if (letterA == letterB)
-                cost = 0;
-            else
-                cost = 1;
-
+            //Get field from the table
             const int diagonal = lineA[j - 1];
             const int left = lineB[j - 1];
             const int above = lineA[j];
 
+            //Calculate correct cost for current number
             int cell = min(above + 1, min(left + 1, diagonal + cost));
             lineB[j] = cell;
         }
 
-        for (int j = 0; j < arr_size; j++)
+        //Move lineB to lineA
+        for (int j = 0; j < lineLength; j++)
             lineA[j] = lineB[j];
     }
 
-    return lineB[arr_size - 1];
-}
-
-
-int parent(int x, vector<int>& id)
-{
-    while(id[x] != x)
-    {
-        id[x] = id[id[x]];
-        x = id[x];
-    }
-
-    return x;
+    //Return last element
+    return lineB[lineLength - 1];
 }
 
 typedef pair<int, int> weightedEdge;
-int kruskal(graph &graph, const size_t& nodesCount) {
+
+//Priority queu Prim algorithm
+int primPQ(graph &graph, const size_t& nodesCount) {
     priority_queue<weightedEdge, vector <weightedEdge>, greater<>> pq;
 
     int src = 0;
@@ -152,11 +154,9 @@ int kruskal(graph &graph, const size_t& nodesCount) {
     }
 
     int sum = 0;
-
     for (int i = 1; i < nodesCount; ++i){
         sum += graph[parent[i]][i];
     }
-
     return sum;
 }
 
