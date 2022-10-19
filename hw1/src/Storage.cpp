30fa00418eeab00fd9f0a8523c1dbdcd84506339
Filip Štěpánek <stepafil@gmail.com>
2022-10-19 19:03:55 +0200

    Working but slow

--- hw1/src/Storage.cpp
+++ hw1/src/Storage.cpp
@@ -1,95 +1,138 @@
 #include <vector>
 #include <numeric>
+#include <algorithm>
+#include <iterator>
 
 #include "Utils.h"
 
-int calculateFrankenstein(const vector<int> &lineA, const vector<int> &lineB);
+typedef vector<pair<int, pair<int, int>>> graphT;
+
+int calculateFrankenstein(const vector<int> &wordA, const vector<int> &wordB);
+
+int kruskal(graphT &edges, const size_t& nodesCount);
+
 
 using namespace std;
+
 
 int main(int argc, char *argv[]) {
     auto programArguments = ProgramArguments::Parse(argc, argv);
 
-    // The input records, e.g., records[0] is the first record in the input file.
     vector<vector<int>> records = readRecords(programArguments.mInputFilePath);
 
-    vector<vector<int>> graph(records.size(), vector<int>(records.size()));
+    const size_t nodesCount = records.size();
 
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < records.size() - 1; ++i) {
-        for (int j = i + 1; j < records.size(); ++j) {
+    auto start = high_resolution_clock::now();
+
+    #pragma omp declare reduction (merge : graphT : omp_out.insert(omp_out.end(), omp_in.begin(), omp_in.end()))
+    int i,j;
+    graphT edges;
+    #pragma omp parallel for schedule(static, 1) private(j) reduction(merge: edges)
+    for (i = 0; i < nodesCount - 1; ++i) {
+        for (j = i + 1; j < nodesCount; ++j) {
             int distance = calculateFrankenstein(records[i], records[j]);
 
-            graph[i][j] = distance;
-            graph[j][i] = distance;
+            edges.push_back(make_pair(distance, make_pair(i, j)));
+            edges.push_back(make_pair(distance, make_pair(j, i)));
         }
     }
 
-    // TODO: fill the treeCost variable with the MST of the records' edit distances graph.
-    int treeCost = 0;
+    auto stopGraph = high_resolution_clock::now();
+    auto durationGraph = duration_cast<milliseconds>(stopGraph - start);
+
+    cout << durationGraph.count() << " ms to create graph" << endl;
+
+    int treeCost = kruskal(edges, nodesCount);
 
     cout << treeCost << endl;
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << duration.count() << " ms to kruskal" << endl;
+
     writeCost(treeCost, programArguments.mOutputFilePath);
+
+    return 0;
 }
 
-int calculateFrankenstein(const vector<int> &lineA, const vector<int> &lineB) {
-    if (lineA.empty()) {
-        return lineB.size();
+int calculateFrankenstein(const vector<int> &wordA, const vector<int> &wordB) {
+    if (wordA.empty()) {
+        return wordB.size();
     }
 
-    if (lineB.empty()) {
-        return lineA.size();
+    if (wordB.empty()) {
+        return wordA.size();
     }
 
-    std::vector<std::vector<int>> matrix(lineA.size() + 1, std::vector<int>(lineB.size()+1));
+    const int arr_size = wordA.size() + 1;
+    int lineA[arr_size];
+    int lineB[arr_size];
 
-    //Fill vertical
-    iota(matrix[0].begin(), matrix[0].end(), 0);
-
-    //Fill horizontal
-    for (int i = 0; i <= lineA.size(); i++) {
-        matrix[i][0] = i;
+    for (int i = 0; i < arr_size; i++) {
+        lineA[i] = i;
     }
 
+    for (int i = 1; i <= wordB.size(); i++) {
+        lineB[0] = i;
+        const int letterB = wordB[i - 1];
 
-    for (int pointerA = 1; pointerA <= lineA.size(); pointerA++) {
+        for (int j = 1; j <= wordA.size(); ++j) {
+            const int letterA = wordA[j - 1];
 
-        const int aMember = lineA[pointerA - 1];
-
-        for (int pointerB = 1; pointerB <= lineB.size(); pointerB++) {
-
-            const char bMember = lineB[pointerB - 1];
 
             int cost;
-            if (aMember == bMember) {
+            if (letterA == letterB)
                 cost = 0;
-            } else {
+            else
                 cost = 1;
-            }
 
-            const int diagonal = matrix[pointerA - 1][pointerB - 1];
-            const int left = matrix[pointerA][pointerB - 1];
-            const int above = matrix[pointerA - 1][pointerB];
+            const int diagonal = lineA[j - 1];
+            const int left = lineB[j - 1];
+            const int above = lineA[j];
 
             int cell = min(above + 1, min(left + 1, diagonal + cost));
+            lineB[j] = cell;
+        }
 
-            //Check transposition
-            if (pointerA > 2 && pointerB > 2) {
-                int transposition = matrix[pointerA - 2][pointerB - 2] + 1;
+        for (int j = 0; j < arr_size; j++)
+            lineA[j] = lineB[j];
+    }
 
-                if (lineA[pointerA - 2] != bMember)
-                    transposition++;
+    return lineB[arr_size - 1];
+}
 
-                if (aMember != lineB[pointerB - 2])
-                    transposition++;
 
-                if (cell > transposition)
-                    cell = transposition;
-            }
+int parent(int x, vector<int>& id)
+{
+    while(id[x] != x)
+    {
+        id[x] = id[id[x]];
+        x = id[x];
+    }
 
-            matrix[pointerA][pointerB] = cell;
+    return x;
+}
+
+int kruskal(graphT &edges, const size_t& nodesCount) {
+    int minimumCost = 0;
+
+    vector<int> id(nodesCount);
+    iota(id.begin(),id.end(),0);
+
+    sort(edges.begin(), edges.end());
+
+    for (int i = 0; i < edges.size(); ++i) {
+        int x = edges[i].second.first;
+        int y = edges[i].second.second;
+        int cost = edges[i].first;
+
+        if (parent(x, id) != parent(y, id)) {
+            minimumCost += cost;
+
+            id[parent(x, id)] = id[parent(y, id)];
         }
     }
 
-    return matrix[lineA.size()][lineB.size()];
+    return minimumCost;
 }
+
